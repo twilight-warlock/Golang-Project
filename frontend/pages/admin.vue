c553740081d3efab11677d27290bf7ac2cbdb550
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-24 00:17:36 +0530

    ft: input for subject

--- frontend/pages/admin.vue
+++ frontend/pages/admin.vue
@@ -4,15 +4,28 @@
       <v-card-title>Admin</v-card-title>
       <v-card-text>
         <v-form @submit.prevent="sendContent">
-          <v-textarea v-model="content" label="Newsletter Content" color="white" filled></v-textarea>
+          <v-text-field
+            v-model="subject"
+            label="Subject"
+            color="white"
+            filled
+          ></v-text-field>
+          <v-textarea
+            v-model="content"
+            label="Newsletter Content"
+            color="white"
+            filled
+          ></v-textarea>
           <!-- <v-checkbox v-model="isHTML" label="Is HTML" value="isHTML" color="white"></v-checkbox> -->
-          <v-btn color="blue darken-1" depressed type="submit">Send to all subscribers!</v-btn>
+          <v-btn color="blue darken-1" depressed type="submit"
+            >Send to all subscribers!</v-btn
+          >
         </v-form>
         <i>Supports HTML!</i>
 
         <br />
 
-        <h1 class="mt-10">Subscribers </h1>
+        <h1 class="mt-10">Subscribers</h1>
         <p>...</p>
       </v-card-text>
     </v-card>
@@ -21,14 +34,17 @@
 
 <script>
 export default {
-    data: () => ({
-        content: '',
-        // isHTML: false,
-    }),
-    methods:{
-        sendContent(){
-            this.$axios.get("https://dog.ceo/api/breeds/image/random").then((res) => console.log(res))
-        }
-    }
+  data: () => ({
+    subject: '',
+    content: '',
+    // isHTML: false,
+  }),
+  methods: {
+    sendContent() {
+      this.$axios
+        .get('https://dog.ceo/api/breeds/image/random')
+        .then((res) => console.log(res))
+    },
+  },
 }
 </script>
