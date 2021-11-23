0729396b55a62f3cab58b3004118e21d34b18582
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-23 23:40:42 +0530

    ft: pages subscribe unsubscribe and admin

---
+++ frontend/pages/admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Admin</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="sendContent">
+          <v-textarea v-model="content" label="Newsletter Content" filled></v-textarea>
+          <v-btn type="submit">Send to all subscribers!</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        content: '',
+    }),
+    methods:{
+        sendContent(){
+            this.$axios.get("https://dog.ceo/api/breeds/image/random").then((res) => console.log(res))
+        }
+    }
+}
+</script>
---
+++ frontend/pages/unsubscribe.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Unsubscribe</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="unsubscribe">
+          <v-text-field v-model="email" label="Email"></v-text-field>
+          <v-btn type="submit">Submit</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        email: '',
+    }),
+    methods:{
+        unsubscribe(){
+            this.$axios.get("https://dog.ceo/api/breeds/image/random").then((res) => console.log(res))
+        }
+    }
+}
+</script>
